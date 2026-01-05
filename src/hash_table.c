c3915e208a81036b934e8f215ac7b84359a3690c
Henry Barber <henrywbarber@gmail.com>
2026-01-05 16:59:39 -0600

    hashing functions

--- src/hash_table.c
+++ src/hash_table.c
@@ -4,6 +4,10 @@
 
 #include "hash_table.h"
 
+#define HT_PRIME_1 151;
+#define HT_PRIME_2 163;
+
+
 static ht_item* ht_new_item(const char* k, const char* v) {
   ht_item* i = malloc(sizeof(ht_item));
   i->key = strdup(k);
@@ -35,4 +39,21 @@ void ht_del_hash_table(ht_hash_table* ht) {
   }
   free(ht->items);
   free(ht);
+}
+
+// Function hashes string `s` via prime `a` (larger than ASCII 128) into bucket < `m`
+static int ht_hash(const char* s, const int a, const int m) {
+  long hash = 0;
+  const int len_s = strlen(s);
+  for (int i = 0; i < len_s; i++) {
+    hash += (long) pow(a, len_s - (i+1)) * s[i];
+    hash = hash % m;
+  }
+  return (int) hash;
+}
+
+static int ht_get_hash(const char* s, const int num_buckets, const int attempt) {
+  const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
+  const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
+  return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
 }
